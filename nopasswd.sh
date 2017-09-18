af191945fc6310e758bc9d0586a3d2a6ebe03bde
qiujieqiong <qiujieqiong@linuxdeepin.com>
2017-09-18 16:04:35 +0800

    use sudo tee -a and echo
    Change-Id: I3bbd773307632ddbd956b5da7f76eddd236868ba

--- nopasswd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-if [ $# != 1 ] ; then 
-				echo "USAGE: sudo bash $0 your_user_name" 
-				echo " e.g.: sudo bash $0 deepin" 
-				exit 1; 
-fi
-
-sh -c "echo '$1 ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
