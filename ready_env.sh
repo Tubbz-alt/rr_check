c743dccfda111c8514b0ebf79778fdd5adaa0426
qiujieqiong <qiujieqiong@linuxdeepin.com>
2017-09-19 16:35:31 +0800

    fix tab error
    Change-Id: Iff0eabc2005475733eeb970c15c5b91c4b3428fc

--- ready_env.sh
+++ ready_env.sh
@@ -5,6 +5,6 @@ echo "$USER ALL=(ALL) NOPASSWD:ALL" |sudo tee -a /etc/sudoers
 sudo apt-get install python3-xlib python3-pip python3-setuptools libwnck-3-dev  python3-tk 
 pip3 install --trusted-host pypi.douban.com -i http://pypi.douban.com/simple/ pandas
 
-cd rr_check
+cd $(PWD)/rr_check
 
 python3 check_pkgs.py
